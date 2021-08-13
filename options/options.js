282a12ec18212add7b15be143f8132309c3290c0
Crowdbotics <team@crowdbotics.com>
2021-08-13 16:14:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "moneyup_29788Identifier",
+  url: "https://moneyup_29788Identifier.botics.co",
+  api: "https://moneyup_29788Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
